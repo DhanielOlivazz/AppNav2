6b733271c5fa33c2f0c0be40945b3d11bd789079
DanielOlivas <juandanielolivasmartinez@gmail.com>
2024-09-28 14:51:10 -0600

    Cambios varios a las interfaces

---
+++ AppNav2/Data/Transactions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppNav2.Data
+{
+    public class Transactions
+    {
+        public string Status { get; set; }
+        public string Date { get; set; }
+        public string Description { get; set; }
+        public string Amount { get; set; }
+
+    }
+}
---
+++ AppNav2/Vistas/Cuentas.xaml.cs
@@ -0,0 +1,9 @@
+namespace AppNav2.Vistas;
+
+public partial class Cuentas : ContentPage
+{
+	public Cuentas()
+	{
+		InitializeComponent();
+	}
+}
